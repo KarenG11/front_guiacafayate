a4d4c8096641ff1bba1bc3441734cf2fe8b5ef5c
Karen Gutierrez <karennilda45@gmail.com>
2026-01-06 17:57:45 -0300

    comente importacion para pwa en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { registerSW } from 'virtual:pwa-register'
+// import { registerSW } from 'virtual:pwa-register'
 import './index.css';
 
-registerSW({ immediate: true })
+// registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
