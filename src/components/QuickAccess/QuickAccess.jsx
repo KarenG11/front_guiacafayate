82a18f17cf36e59cadb61c68f5f05a56e06e6422
Karen Gutierrez <karennilda45@gmail.com>
2026-01-08 11:37:52 -0300

    cambio

--- src/components/QuickAccess/QuickAccess.jsx
+++ src/components/QuickAccess/QuickAccess.jsx
@@ -22,7 +22,7 @@ function QuickAccess() {
       </section>
     );
   }
-
+//cambio
   return (
     <section className="quick-access">
       <h2 className="quick-access-title">¿Qué estás buscando?</h2>
