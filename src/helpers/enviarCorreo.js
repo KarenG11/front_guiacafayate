b356d9a415b881def4b203e5871cc3f9e8d5aac5
Karen Gutierrez <karennilda45@gmail.com>
2026-01-09 15:43:00 -0300

    correccion en enviar correo js

--- src/helpers/enviarCorreo.js
+++ src/helpers/enviarCorreo.js
@@ -1,15 +1,16 @@
 const URL_MAILER = import.meta.env.VITE_API_MAILER;
 
 export const enviarCorreo = async (datos) => {
-  try {
-    const response = await fetch(URL_MAILER, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(datos),
-    });
-  } catch (error) {
-    console.error("Error al enviar el correo:", error);
+  const response = await fetch(URL_MAILER, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(datos),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json().catch(() => ({})); // Intenta obtener detalles del error
+    throw new Error(errorData.message || `Error del servidor: ${response.status}`);
   }
 };
